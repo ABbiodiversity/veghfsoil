f5d893e426e4ae5e018503bd5672c7f3eb8ffb6f
Marcus Becker <mabecker@ualberta.ca>
2022-11-24 12:56:15 -0700

    add hf type and category lookup tables to the inst/extdata folder

--- data-raw/hflt.R
+++ data-raw/hflt.R
@@ -1,8 +1,10 @@
 ## code to prepare `hflt` dataset goes here
 
-hftypes <- read.csv("lookup-hf-type-v2014.csv")
+library(veghfsoil)
+system.file("extdata", package = "veghfsoil") |> list.files()
+hftypes <- read.csv("./inst/extdata/lookup-hf-type-v2014.csv")
 hftypes <- droplevels(hftypes[!is.na(hftypes$HF_GROUP_COMB) & !duplicated(hftypes$FEATURE_TY),])
-hfgroups <- read.csv("lookup-hf-class-v2014.csv")
+hfgroups <- read.csv("./inst/extdata/lookup-hf-class-v2014.csv")
 hflt <- hfgroups[match(hftypes$HF_GROUP_COMB, hfgroups$HF_GROUP_COMB),]
 rownames(hflt) <- hftypes$FEATURE_TY
 
